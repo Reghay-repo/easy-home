390e9f8a65a36fbd56fe1a44b2bdc187abe6505d
Oussama <osm2014@hotmail.fr>
2022-07-07 18:45:18 +0100

    Fix Add slider and arrange project

---
+++ index.js
@@ -0,0 +1,37 @@
+const express       = require('express')
+
+const path          = require('path')
+const port          = 3000
+const app           = express()
+const ejsMate = require('ejs-mate')
+
+
+
+
+
+
+// set ejs as view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs',ejsMate )
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+
+
+
+
+
+
+
+// route to home
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+
+
+
+// setup server on port 3000
+app.listen(port, () => {
+    console.log(`serving your app on http://localhost:${port}`)
+})
